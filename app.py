028580bb0bb8bc2c0f1d897b6a872677b1d137a2
saniyaMeshram <meshramsaniya03@gmail.com>
2026-09-21 11:40:30 +0530

    Fix button condition for prediction

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ petal_length = st.number_input('Petal Length (cm)', min_value=0.0 , max_value=8.
 petal_width = st.number_input('Petal Width (cm)', min_value=0.0 , max_value=10.0 ,value=1.0, step=0.1)
 
 #prediction
- if st.button('predict'):
+if st.button('predict'):
   input_data =np.array([[sepal_length,
                          sepal_width,
                          petal_length,
