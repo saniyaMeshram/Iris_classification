e5d78bd9258f3788263d4fe68f3a7f05fab33658
saniyaMeshram <meshramsaniya03@gmail.com>
2026-09-21 11:30:47 +0530

    Fix input step values for Iris flower measurements

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ st.title('Iris Flower Prediction App')
 st.header('Enter the measurements of the Iris Flower')
 
 #input Labels
-sepal_length = st.number_input('Sepal Length (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0,1)
-sepal_width = st.number_input('Sepal Width (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0,1)
-petal_length = st.number_input('Petal Length (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0,1)
-petal_width = st.number_input('Petal Width (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0,1)
+sepal_length = st.number_input('Sepal Length (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0.1)
+sepal_width = st.number_input('Sepal Width (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0.1)
+petal_length = st.number_input('Petal Length (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0.1)
+petal_width = st.number_input('Petal Width (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0.1)
 
 #prediction
 if st.button('predict'):
