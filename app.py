eced393489ad491d2279346d89ce37ab57250155
saniyaMeshram <meshramsaniya03@gmail.com>
2026-09-21 11:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-import numpy as pd
+import numpy as np
 
 #load model
 model = joblib.load('iris_model.pkl')
@@ -14,7 +14,7 @@ st.header('Enter the measurements of the Iris Flower')
 sepal_length = st.number_input('Sepal Length (cm)', min_value=0.0 , max_value=10.0 ,value=5.0, step=0.1)
 sepal_width = st.number_input('Sepal Width (cm)', min_value=0.0 , max_value=10.0 ,value=3.0, step=0.1)
 petal_length = st.number_input('Petal Length (cm)', min_value=0.0 , max_value=8.0 ,value=4.0, step=0.1)
-petal_width = st.number_input('Petal Width (cm)', min_value=0.0 , max_value-10.0 ,value=1.0, step=0.1)
+petal_width = st.number_input('Petal Width (cm)', min_value=0.0 , max_value=10.0 ,value=1.0, step=0.1)
 
 #prediction
  if st.button('predict'):
