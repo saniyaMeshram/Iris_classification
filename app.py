4b6843a85a05552e737747b43a438055f259c2db
saniyaMeshram <meshramsaniya03@gmail.com>
2026-09-21 11:23:33 +0530

    Implement Iris Flower Prediction App
    Fix input field errors and prediction logic.

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import joblib
+import numpy as pd
+
+#load model
+model = joblib.load('iris_model.pkl')
+
+#page title
+st.title('Iris Flower Prediction App')
+
+st.header('Enter the measurements of the Iris Flower')
+
+#input Labels
+sepal_length = st.number_input('Sepal Length (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0,1)
+sepal_width = st.number_input('Sepal Width (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0,1)
+petal_length = st.number_input('Petal Length (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0,1)
+petal_width = st.number_input('Petal Width (cm)' , min_value=0.0 , mav _value-10.0 ,value=5.0, step=0,1)
+
+#prediction
+if st.button('predict'):
+  input_data =np.array([[sepal_length,
+                         sepal_width,
+                         petal_length,
+                         petal_width]]).astype(np.float64)
+  prediction =model.predict(input.data)
+  st.success(f'The predicted Iris Species is: {prediction[0}')
+                         
+                         
+                         
